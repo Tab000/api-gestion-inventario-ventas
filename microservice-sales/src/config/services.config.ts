bb0c2c3d1a859b71781a95ffed43e8be87c0c269
Nicolás Taborda <thanosntm2007@gmail.com>
2025-11-20 15:04:02 -0500

    Delete microservice-sales directory

--- microservice-sales/src/config/services.config.ts
+++
@@ -1,5 +0,0 @@
-export const SERVICES_CONFIG = {
-    CLIENTS_SERVICE_URL: process.env.CLIENTS_SERVICE_URL || 'http://localhost:3001',
-    PRODUCTS_SERVICE_URL: process.env.PRODUCTS_SERVICE_URL || 'http://localhost:3002'
-};
-
